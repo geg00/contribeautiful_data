861db5cb05e9501000351347d244b5d6fbc6c95e
geg00 <gerardo.e.garcia@gmail.com>
2021-12-03 09:50:44 -0600

    Updates

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 import gradio as gr
+from github import Github
 
 def greet(name):
+    g = Github()
+    repo = g.get_repo("huggingface/transformers")
+    open_issues = repo.get_issues(state='open')
+    for issue in open_issues:
+        print ("Title:",issue.title, "Body:", issue.body , "Labels:", issue.labels)
     return "Hello " + name + "!!"
 
 iface = gr.Interface(fn=greet, inputs="text", outputs="text")
